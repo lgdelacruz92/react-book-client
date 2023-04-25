a2ccc24f4cd51a0b5efba786efb224385b09023c
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-04-25 17:17:23 -0400

    i can make stuff

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,12 +1,54 @@
-import { Box, Stack } from "@chakra-ui/react";
-import MarkDown from "./components/mark-down/mark-down";
+import { Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+import { StreamChat } from "stream-chat";
+import {
+  Attachment,
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Window,
+} from "stream-chat-react";
+import { DefaultStreamChatGenerics } from "stream-chat-react/dist/types/types";
+
+import "stream-chat-react/dist/css/v2/index.css";
+
+const chatClient = new StreamChat("nf84egcdpf3m");
+const userToken =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoid2VhdGhlcmVkLW1vcm5pbmctMSJ9.yeOriF7jOZlrPpFpF5S1zZUg7GiEVwbU5vCRxq5ql_k";
+
+chatClient.connectUser(
+  {
+    id: "weathered-morning-1",
+    name: "weathered-morning-1",
+    image:
+      "https://getstream.io/random_png/?id=weathered-morning-1&name=weathered-morning-1",
+  },
+  userToken
+);
+
+const channel = chatClient.channel("messaging", "custom_channel_id", {
+  // add as many custom fields as you'd like
+  image: "https://www.drupal.org/files/project-images/react.png",
+  name: "Talk about React",
+  members: ["weathered-morning-1"],
+  attachments: false,
+});
 
 const Sidebar = () => {
   return (
     <Box>
-      <Stack spacing={4} p={4}>
-        <MarkDown content="This is a line\n\nThis is another."/>
-      </Stack>
+      <Chat client={chatClient} theme="messaging light">
+        <Channel channel={channel} Attachment={Attachment}>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput noFiles />
+          </Window>
+        </Channel>
+      </Chat>
     </Box>
   );
 };
