fb72c036fc091cc34edc12c254daac394ea477a3
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-04-26 17:06:01 -0400

    alright not bad

--- src/lib/stream-chat.ts
+++ src/lib/stream-chat.ts
@@ -2,6 +2,9 @@ const StreamChat = {
   getToken: async (userid: string) => {
     return await fetch(`${process.env.API_URL}/streamchat/token`, {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({ user: userid }),
     });
   },
--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -12,42 +12,37 @@ import {
   MessageList,
   Window,
 } from "stream-chat-react";
-import { DefaultStreamChatGenerics } from "stream-chat-react/dist/types/types";
 
 import "stream-chat-react/dist/css/v2/index.css";
 import GoogleSignOutButton from "./components/google/google-signout-button";
 import { useRouter } from "next/router";
-import {
-  UseStreamChatChannelProps,
-  UseStreamChatClientProps,
-} from "@/hooks/stream-chat/stream-chat.types";
+import { UseStreamChatClientProps } from "@/hooks/stream-chat/stream-chat.types";
 import { useStreamChatClient } from "@/hooks/stream-chat/use-stream-chat-client";
 import { useStreamChatChannel } from "@/hooks/stream-chat/use-stream-chat-channel";
-const apiKey = process.env.STREAMCHAT_API_KEY || "";
-
-// TODO: get this from backend
-// const userId = "lester2";
-// const userToken =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGVzdGVyMiJ9.AcZiVADB4RIK4eNDJ_nd1dE4rcjdG3Xu8fdk5sfqqVk";
 
 const Playground = () => {
   const router = useRouter();
   const [user, setUser] = useState<UseStreamChatClientProps | null>();
-  const [channel, setChannel] = useState<UseStreamChatChannelProps | null>();
 
-  const { chatClient } = useStreamChatClient({ userId: user?.userId });
+  const { chatClient } = useStreamChatClient({
+    userId: user?.userId,
+    userToken: user?.userToken,
+  });
   const { chatChannel } = useStreamChatChannel({
     client: chatClient,
-    channel: channel?.channel,
-    channelOptions: channel?.channelOptions,
+    channel: user?.userId,
+    channelOptions: {
+      name: "AI-Tutor",
+      members: [user?.userId || ""],
+    },
   });
 
   useEffect(() => {
-    if (!AppFirebase.auth().currentUser) {
-      router.push("signin");
-      return;
-    }
-
+    const unsubscribe = AppFirebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        router.push("/signin");
+      }
+    });
     const initializeUser = async (userId: string) => {
       if (userId.length > 0) {
         const tokenResponse = await AppStreamChat.getToken(userId);
@@ -58,12 +53,9 @@ const Playground = () => {
         });
       }
     };
-
     initializeUser(AppFirebase.auth().currentUser?.uid || "");
 
-    // console.log(AppFirebase.auth().currentUser);
-    // console.log(AppFirebase.auth().currentUser?.uid);
-    // console.log(AppFirebase.auth().currentUser?.email);
+    return unsubscribe;
   }, []);
 
   if (!chatClient || !chatChannel) {
