1395922d0d045840ed0462a48a2d089424a5005d
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-04-26 08:38:44 -0400

    I have a working prototype of chat system

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -16,12 +16,15 @@ import { DefaultStreamChatGenerics } from "stream-chat-react/dist/types/types";
 import "stream-chat-react/dist/css/v2/index.css";
 const apiKey = process.env.STREAMCHAT_API_KEY || "";
 
-interface UseChatClientProps {
+interface UseStreamChatClientProps {
   userId: string;
   userToken: string;
 }
 
-const useChatClient = ({ userId, userToken }: UseChatClientProps) => {
+const useStreamChatClient = ({
+  userId,
+  userToken,
+}: UseStreamChatClientProps) => {
   const [chatClient, setChatClient] =
     useState<StreamChat<DefaultStreamChatGenerics>>();
 
@@ -40,36 +43,57 @@ const useChatClient = ({ userId, userToken }: UseChatClientProps) => {
   };
 };
 
+interface UseStreamChatChannelProps {
+  channel: string;
+  channelOptions: Record<string, string | string[]>;
+  client?: StreamChat<DefaultStreamChatGenerics>;
+}
+
+const useStreamChatChannel = ({
+  channel,
+  channelOptions,
+  client,
+}: UseStreamChatChannelProps) => {
+  const [chatChannel, setChatChannel] =
+    useState<StreamChatChannel<DefaultStreamChatGenerics>>();
+
+  useEffect(() => {
+    const initializeChannel = async () => {
+      if (client) {
+        const connectedChannel = client.channel(
+          "messaging",
+          channel,
+          channelOptions
+        );
+        await connectedChannel.watch();
+        setChatChannel(connectedChannel);
+      }
+    };
+    initializeChannel();
+  }, [client]);
+
+  return { chatChannel };
+};
+
 // TODO: get this from backend
 const userId = "lester";
 const userToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGVzdGVyIn0.vl4k9H646Deg5-mAWQevIz1Mr7gm22sp1CPymE_u_bQ";
 
 const Sidebar = () => {
-  const { chatClient } = useChatClient({ userId, userToken });
-  const [chatChannel, setChatChannel] =
-    useState<StreamChatChannel<DefaultStreamChatGenerics>>();
-
-  useEffect(() => {
-    const initializeChannel = async () => {
-      if (chatClient) {
-        const channel = chatClient.channel("messaging", "new_channel", {
-          // add as many custom fields as you'd like
-          image: "https://www.drupal.org/files/project-images/react.png",
-          name: "Talk about React",
-          members: ["lester"],
-        });
-        await channel.watch();
-        setChatChannel(channel);
-      }
-    };
-    initializeChannel();
-  }, [chatClient]);
-
-  useEffect(() => {
-    if (chatChannel) {
-    }
-  }, [chatChannel]);
+  const channel = "new_channel";
+  const channelOptions = {
+    // add as many custom fields as you'd like
+    image: "https://www.drupal.org/files/project-images/react.png",
+    name: "Talk about React",
+    members: ["lester"],
+  };
+  const { chatClient } = useStreamChatClient({ userId, userToken });
+  const { chatChannel } = useStreamChatChannel({
+    client: chatClient,
+    channel,
+    channelOptions,
+  });
 
   if (!chatClient || !chatChannel) {
     return null;
