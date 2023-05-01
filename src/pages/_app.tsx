0466eacef8197543dcd60c66a8ca10f0edb5e723
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-05-01 10:04:12 -0400

    using vercel for deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import "@/styles/editor.css";
 
 import { type AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
