a5dd7d0101af52cf7e0f8875a5aead9e0996241f
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-05-04 07:09:04 -0400

    added stripe redirect

--- src/services/stripe.service.ts
+++ src/services/stripe.service.ts
@@ -1,4 +1,17 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY || "");
-export { stripePromise };
+
+const redirectToCheckout = async (sessionId: string) => {
+  try {
+    const stripe = await stripePromise;
+    if (!stripe) {
+      throw Error(`Stripe object null: ${stripe}`);
+    }
+    await stripe.redirectToCheckout({ sessionId });
+  } catch (error) {
+    console.error("Error redirecting to Checkout:", error);
+  }
+};
+
+export { stripePromise, redirectToCheckout };
