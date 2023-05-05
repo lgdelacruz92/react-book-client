040dd169e1d623fcabcc210801ba0b3e7d377588
Lester <lgdelacruz@Lesters-MacBook-Pro.local>
2023-05-05 16:04:29 -0400

    complete

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,8 @@ import "@/styles/editor.css";
 import { type AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect, useState } from "react";
-import { getSessionKey } from "@/lib/firebase";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { getUser } from "@/services/user.service";
 import { getSession } from "@/services/session.service";
 
 export default function App({ Component, pageProps }: AppProps) {
